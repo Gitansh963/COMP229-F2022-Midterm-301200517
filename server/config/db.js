5c40715c589528121158b75967f342883b91da66
Gitansh963 <gitansh9mittal@gmail.com>
2022-10-26 19:32:09 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    // "URI":"mongodb://127.0.0.1:27017/book_store",
+    "URI": "mongodb+srv://gitansh2002:gitansh2002@assignment2.bgdbzkl.mongodb.net/?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
+// module.exports = {
+//     //local MongoDB deployment ->
+//     // "LocalURI": "mongodb://127.0.0.1/books229",
+//     "RemoteURI": "mongodb+srv://gitansh2002:gitansh2002@assignment2.bgdbzkl.mongodb.net/?retryWrites=true&w=majority",
+//     // "HostName": "LocalHost",
+//     "Secret": "SomeSecret"
+//   };
+  
